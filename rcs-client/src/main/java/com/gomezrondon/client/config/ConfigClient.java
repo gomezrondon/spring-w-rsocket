8350efa9eb7cd404b3062a55c62a52c940fa85d6
gomezrondon <gomezrondon@gmail.com>
2020-09-16 15:30:24 -0600

    Decupling the code from the application entry to a config class.

---
+++ rcs-client/src/main/java/com/gomezrondon/client/config/ConfigClient.java
@@ -0,0 +1,40 @@
+package com.gomezrondon.client.config;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import reactor.util.retry.Retry;
+
+import java.io.IOException;
+import java.time.Duration;
+
+@Configuration
+@Slf4j
+public class ConfigClient {
+
+/*    @Bean
+    ApplicationListener<ApplicationReadyEvent> rsocketRequestListerner(RSocketRequester requester) {
+        return event -> {
+            requester
+                    .route("test-time") // returns a stream
+                    //.route("solo-texto") // returns 1 value
+                    .data("Test")
+                    .retrieveFlux(String.class) // or retrieveMono
+                    .retryWhen(Retry.backoff(3, Duration.ofSeconds(5) ))
+                    .doOnError(IOException.class, e -> log.error(e.getMessage()))
+                    .subscribe(System.out::println);
+        };
+    }*/
+
+    @Bean
+    RSocketRequester rSocketRequester(RSocketRequester.Builder builder) {
+        return builder
+                .connectTcp("localhost", 8888)
+                .block();
+    }
+
+}
